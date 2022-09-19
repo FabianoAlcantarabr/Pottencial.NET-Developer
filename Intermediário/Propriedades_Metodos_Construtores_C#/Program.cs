47cb6848a6d30e07082cc0770fdef9d523749037
Fabiano Alcantara <fabianoalcantarabr@yahoo.com.br>
2022-09-19 12:10:28 -0300

    Iniciando o Módulo Intermediário

---
+++ Intermediário/Propriedades_Metodos_Construtores_C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using Propriedades_Metodos_Construtores_C_.Models;
+
+Pessoa p1 = new Pessoa(nome: "Fabiano", sobrenome: "Alcantara");
+Pessoa p2 = new Pessoa(nome: "Fabio", sobrenome: "Santos");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Alcantara";
+// p1.Sobrenome = "Santos";
+// p1.Idade = 35;
+// p1.Apresentar();
