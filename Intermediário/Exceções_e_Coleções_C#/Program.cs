fdd7ba0ceda2fdd2c4509ddb394f29a94355b5b1
Fabiano Alcantara <fabianoalcantarabr@yahoo.com.br>
2022-09-20 12:49:36 -0300

    Começando a Tarefa Exceções e Coleções em C#

---
+++ Intermediário/Exceções_e_Coleções_C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exceções_e_Coleções_C_.Models;
+using System.Globalization;
+
+
+try  // usa para conferir se o arquivo existe e se não existir ele não para o programa só avisa
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+
+}catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
