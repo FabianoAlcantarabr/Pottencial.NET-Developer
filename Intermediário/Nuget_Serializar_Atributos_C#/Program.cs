540f8b2139d40425df8753d00ffe55aeb73fa38e
Fabiano Alcantara <fabianoalcantarabr@yahoo.com.br>
2022-09-21 11:56:12 -0300

    Conclusão da Tarefa Nuget e Serializar em C#

--- Intermediário/Nuget_Serializar_Atributos_C#/Program.cs
+++ Intermediário/Nuget_Serializar_Atributos_C#/Program.cs
@@ -1,22 +1,45 @@
 ﻿using Nuget_Serializar_Atributos_C_.Models;
 using System.Globalization;
+using Newtonsoft.Json;
 
-/// IF Ternário ////
-int numeros = 25;
-bool ehPar = false;
+//// Deserializando um objeto /////
 
-ehPar = numeros % 2 == 0;
-Console.WriteLine($"O número {numeros} é " + (ehPar ? "par" : "ímpar"));
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json"); //  Leu todo o conteudo do Arquivo
 
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);  // Pega em Json e tranforma em Objeto com o DeserializeObject
 
-//  Código normal
-int numero = 18;     
-
-if (numero % 2 == 0)
+foreach(Venda venda in listaVenda)
 {
-    Console.WriteLine($"O número {numero} é par");
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
 }
-else
-{
-    Console.WriteLine($"O número {numero} é ímpar");
-}
+
+
+
+/// Serializando um Código ///////
+
+// DateTime dataAtual = DateTime.Now;
+// List<Venda> listaVendas = new List<Venda>();
+
+// Venda v1 = new Venda(1, "Caderno", 15.00M, dataAtual);
+// Venda v2 = new Venda(2, "Licença de Software", 150.00M, dataAtual);
+// Venda v3 = new Venda(3, "Caneta", 5.00M, dataAtual);
+
+// listaVendas.Add(v1);
+// listaVendas.Add(v2);
+// listaVendas.Add(v3);
+
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+
+// Console.WriteLine(serializado);
+
+
+
+
+
+
+
+
+
+
