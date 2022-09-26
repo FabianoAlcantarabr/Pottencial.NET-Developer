1234c8b764047993ef9fe654a8c645e6e2c48bd4
Fabiano Alcantara <fabianoalcantarabr@yahoo.com.br>
2022-09-26 13:30:00 -0300

    Iniciando o modulo de Orientação a objetos

---
+++ Orientação a Objetos_C#/Int_Abstração_Encapsulamento_C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using Int_Abstração_Encapsulamento_C_.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(2000);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Fabiano";
+// p1.Idade = 30;
+
+// p1.Apresentar();
